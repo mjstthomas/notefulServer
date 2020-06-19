3e8e18d1450c472f12cab08a0c4653ca10970d0d
MJ <mjstthomas9516@gmail.com>
2020-06-18 17:36:04 -0700

    added api token

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN = process.env.API_TOKEN,
   DATABASE_URL: process.env.DATABASE_URL || 'postgres://ggyzoszwamkmqf:f88dd8afe25dbe194a4d17dcdb1668b5377ad69d7adf1a4307e1e45b96f303bd@ec2-34-193-117-204.compute-1.amazonaws.com:5432/d9ljdd9m6d67a2'
 }
